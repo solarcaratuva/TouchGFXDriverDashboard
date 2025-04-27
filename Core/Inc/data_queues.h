b8f468f918a3c63d72b4af3005f224d046fa261c
Arleee1 <keg9ve@virginia.edu>
2025-04-27 16:26:40 -0400

    add can struct unpacking

--- Core/Inc/data_queues.h
+++ Core/Inc/data_queues.h
@@ -4,16 +4,49 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 #include "FreeRTOS.h"
 #include "queue.h"
+#include "stdint.h"
+#include "rivanna3.h"
+#include "bps.h"
+#include "motor_controller.h"
 
 extern QueueHandle_t canReceivedQueue;
 
+/**
+ * Used to pass data from CAN to the display
+ */
 typedef struct {
-  int test;
+     /**
+      * Definition found in rivanna3.h
+      */
+     struct rivanna3_motor_commands_t motor_commands;
+
+     /**
+      * Definition found in bps.h
+      */
+     struct bps_bps_pack_information_t bps_pack_information;
+     struct bps_bps_error_t bps_error;
+
+     /**
+      * Definition found in motor_controller.h
+      */
+     struct motor_controller_motor_controller_power_status_t motor_controller_power_status;
+     struct motor_controller_motor_controller_error_t motor_controller_error;
+
+    
 } ReceivedCanData_t;
 
+/**
+ * Initializes the queues
+ */
 void setupDataQueues();
+
+/**
+ * Updates the shared CAN data struct with new data
+ */
+void updateReceivedCanData(ReceivedCanData_t *oldData, uint32_t Identifier, uint8_t *RxData);
 
 #ifdef __cplusplus
 }
--- Core/Src/data_queues.c
+++ Core/Src/data_queues.c
@@ -1,7 +1,35 @@
 #include "data_queues.h"
 
 QueueHandle_t canReceivedQueue;
+ReceivedCanData_t canReceivedQueueStorageBuffer;
+StaticQueue_t canReceivedQueueOverheadBuffer;
 
 void setupDataQueues() {
-  canReceivedQueue= xQueueCreate(1, sizeof(ReceivedCanData_t));
+  canReceivedQueue = xQueueCreateStatic(1, sizeof(ReceivedCanData_t), (uint8_t*) &canReceivedQueueStorageBuffer, &canReceivedQueueOverheadBuffer);
 }
+
+#define RxDataSize 8
+
+void updateReceivedCanData(ReceivedCanData_t *oldData, uint32_t Identifier, uint8_t *RxData) {
+  switch(Identifier) {
+    case RIVANNA3_MOTOR_COMMANDS_FRAME_ID:
+      rivanna3_motor_commands_unpack(&oldData->motor_commands, RxData, RxDataSize);
+      break;
+    case BPS_BPS_PACK_INFORMATION_FRAME_ID:
+      bps_bps_pack_information_unpack(&oldData->bps_pack_information, RxData, RxDataSize);
+      break;
+    case BPS_BPS_ERROR_FRAME_ID:
+      bps_bps_error_unpack(&oldData->bps_error, RxData, RxDataSize);
+      break;
+    case MOTOR_CONTROLLER_MOTOR_CONTROLLER_POWER_STATUS_FRAME_ID:
+      motor_controller_motor_controller_power_status_unpack(&oldData->motor_controller_power_status, RxData, RxDataSize);
+      break;
+    case MOTOR_CONTROLLER_MOTOR_CONTROLLER_ERROR_FRAME_ID:
+      motor_controller_motor_controller_error_unpack(&oldData->motor_controller_error, RxData, RxDataSize);
+      break;
+    default:
+      break;
+  }
+}
+
+#undef RxDataSize
